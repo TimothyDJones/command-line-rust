b24b550202ecfd3ca658a393201afd2ffbdf9601
Tim Jones <tdjones74021@yahoo.com>
2023-12-20 12:44:29 -0600

    Add code for book chapter 01.

---
+++ ch01/hello/tests/cli.rs
@@ -0,0 +1,27 @@
+// use std::process::Command;
+use assert_cmd::Command;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    // let res = cmd.output();
+    // assert!(res.is_ok());
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
